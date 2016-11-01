a4fef0a28e96d121b317ddb4e3f7ef4cf3ab4bc2
fbell123 <fbell.pro@gmail.com>
2016-11-01 10:34:51 +0000

    resolve review comments

--- spec/features/new_space_spec.rb
+++ spec/features/new_space_spec.rb
@@ -1,7 +1,6 @@
 feature 'Add a new space' do
   scenario 'A host can view the page to add a new space' do
     visit '/spaces/new'
-    expect(page.status_code).to eq 200
     expect(page).to have_content("Add a new listing")
     expect(page).to have_selector(:link_or_button, 'Save')
   end
