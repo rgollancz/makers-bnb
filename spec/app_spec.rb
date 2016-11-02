91f61fce322689b4e1ab32ef049a54d90a8e74bb
Chris Lawrence <chris.lawrence@gmx.com>
2016-11-02 10:48:56 +0000

    minor refactor

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -28,7 +28,6 @@ describe Makersbnb do
   describe 'post /spaces' do
     it 'posts a new space' do
       post '/spaces', user_id: 1, name: "One", address: "14 Two Lane", description: "It's a house", price: 130.64
-      # post '/spaces', name: "One", address: "14 Two Lane", description: "It's a house", price: 130.64
       expect(last_response.redirect?).to be true
       follow_redirect!
       expect(last_request.path).to eq("/spaces")
