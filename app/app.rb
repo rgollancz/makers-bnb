ce82e60224aafd33df242316f2f5a68cc843ae12
fbell123 <fbell.pro@gmail.com>
2016-11-04 11:36:18 +0000

    approve/reject booking in progress

--- app/app.rb
+++ app/app.rb
@@ -65,6 +65,13 @@ class Makersbnb < Sinatra::Base
     end
   end
 
+  get '/spaces/:id' do
+    @space = Space.get(params[:id])
+    @space_id = params[:id]
+    @failed = params[:failed]
+    erb :'spaces/individual'
+  end
+
   get '/bookings' do
     if current_user
       @bookings_requested = current_user.bookings
@@ -76,18 +83,10 @@ class Makersbnb < Sinatra::Base
     end
   end
 
-  get '/spaces/:id' do
-    @space = Space.get(params[:id])
-    @space_id = params[:id]
-    @failed = params[:failed]
-    erb :'spaces/individual'
-  end
-
-
   post '/bookings/:space_id' do
     @booking = Booking.create(start_date: params[:start_date],
                               end_date: params[:end_date],
-                              status: "unconfirmed",
+                              status: "Pending",
                               user_id: current_user.id,
                               space_id: params[:space_id])
     redirect to '/bookings'
--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -6,18 +6,23 @@ feature 'View requests' do
     new_user_2
     new_booking_user_id_2
     visit '/bookings'
-    expect(page).to have_content("My bookings")
-    expect(page).to have_content("Two")
+    expect(page).to have_content('My bookings')
+    expect(page).to have_content('Two')
   end
 
-  scenario 'A host can see who the request is made by' do
+  scenario 'A host can see who the request is made by and confirm or reject' do
     host_client_booking_setup
     login_user_1
     visit '/bookings'
-    expect(page).to have_content("approval for Mary")
+    expect(page).to have_content('for Mary')
+    expect(page).to have_selector(:link_or_button, 'Confirm' && 'Reject')
   end
 
   scenario 'host can confirm or reject booking request' do
-    
+    host_client_booking_setup
+    login_user_1
+    visit '/bookings'
+    click_button 'Confirm'
+    expect(page).to have_content('Approved')
   end
 end
