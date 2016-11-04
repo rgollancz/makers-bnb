f2bce6d81914211d4213a0ea03afc78828cf9509
fbell123 <fbell.pro@gmail.com>
2016-11-04 16:21:03 +0000

    refactored tests

--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -10,18 +10,11 @@ feature 'View requests' do
     expect(page).to have_content('Two')
   end
 
-  scenario 'A host can see who the request is made by and confirm or reject' do
+  scenario 'A host can see who the request is made by' do
     host_client_booking_setup
     login_user_1
     visit '/bookings'
     expect(page).to have_content('for Mary')
   end
 
-  # scenario 'host can confirm or reject booking request' do
-  #   host_client_booking_setup
-  #   login_user_1
-  #   visit '/bookings'
-  #   click_button 'Confirm'
-  #   expect(page).to have_content('Approved')
-  # end
 end
